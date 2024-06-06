48d04f2f7e0d2b80d12fee2bc269792e70f87c6e
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-06-06 16:27:53 -0300

    aulas da seção 1 até a 6

---
+++ resources/views/site/sobre-nos.blade.php
@@ -0,0 +1,7 @@
+<h1>Sobre-nós (view)</h1>
+
+<ul>
+    <li><a href="{{route('site.index')}}">Principal</a></li>
+    <li><a href="{{route('site.sobrenos')}}">Sobre-nós</a></li>
+    <li><a href="{{route('site.contato')}}">Contato</a></li>
+</ul>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    })->name('app.produtos');
+});
