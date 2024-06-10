794b2c2f769f7007d015c5aea9f1554551821d99
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-06-10 13:14:32 -0300

    Seção 11 - Implementando formulário de login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
@@ -14,9 +15,8 @@ Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.so
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 
-Route::get('/login', function () {
-    return 'Login';
-})->name('site.login');
+Route::get('/login', [LoginController::class,'index'])->name('site.login');
+Route::post('/login', [LoginController::class,'autenticar'])->name('site.login');
 
 Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function () {
     Route::get('/clientes', function () {return 'Clientes'; })->name('app.clientes');
