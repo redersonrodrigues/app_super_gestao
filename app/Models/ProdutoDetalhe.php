0e589f866e5ddeed1a9e7f18e70733945a93ddc1
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-06-12 08:52:39 -0300

    Seção 12 aula 180. Eloquent ORM 1 para 1 - Exibindo informações do produto (belongsTo)

--- app/Models/ProdutoDetalhe.php
+++ app/Models/ProdutoDetalhe.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProdutoDetalhe extends Model
 {
@@ -11,4 +12,14 @@ class ProdutoDetalhe extends Model
     protected $fillable =[
         'produto_id', 'comprimento', 'largura', 'altura', 'unidade_id'
     ];
+
+    /**
+     * Get the produto that owns the ProdutoDetalhe
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function produto(): BelongsTo
+    {
+        return $this->belongsTo(Produto::class);
+    }
 }
