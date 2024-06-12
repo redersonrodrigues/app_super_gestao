d0aceec7d258cf7260dd85d678b82719da2b54c8
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-06-12 06:55:53 -0300

    Seção 12 - aula 175 - Eloquent ORM 1 para 1 - Implementando produto detalhes parte 1

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\ProdutoDetalheController;
 use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,16 +19,16 @@ Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.so
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 
-Route::get('/login/{erro?}', [LoginController::class,'index'])->name('site.login');
-Route::post('/login', [LoginController::class,'autenticar'])->name('site.login');
+Route::get('/login/{erro?}', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 
 Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function () {
-    Route::get('/home', [HomeController::class,'index'])->name('app.home');
-    Route::get('/sair', [LoginController::class, 'sair'])->name('app.sair');    
+    Route::get('/home', [HomeController::class, 'index'])->name('app.home');
+    Route::get('/sair', [LoginController::class, 'sair'])->name('app.sair');
     Route::get('/clientes', [ClienteController::class, 'index'])->name('app.cliente');
 
     Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
-    
+
     Route::post('/fornecedor/listar', [FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
     Route::get('/fornecedor/listar', [FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
 
@@ -35,12 +36,15 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
     Route::get('/fornecedor/adicionar', [FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
     Route::post('/fornecedor/adicionar', [FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
 
-    Route::get('/fornecedor/editar/{id}/{msg?}',[FornecedorController::class,'editar'])->name('app.fornecedor.editar');
+    Route::get('/fornecedor/editar/{id}/{msg?}', [FornecedorController::class, 'editar'])->name('app.fornecedor.editar');
 
-    Route::get('/fornecedor/excluir/{id}/{msg?}',[FornecedorController::class,'excluir'])->name('app.fornecedor.excluir');
+    Route::get('/fornecedor/excluir/{id}/{msg?}', [FornecedorController::class, 'excluir'])->name('app.fornecedor.excluir');
 
     //produtos
     Route::resource('/produto', ProdutoController::class);
+
+    //produto-detalhe
+    Route::resource('/produto-detalhe', ProdutoDetalheController::class);
 });
 
 Route::fallback(function () {
